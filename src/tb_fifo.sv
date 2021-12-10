479622f7f1494f54dfacee711646cc732465861a
Alexis <alexis.sbtg@gmail.com>
2021-12-09 19:47:28 -0600

    All files compiled, pending tests

--- src/tb_fifo.sv
+++ src/tb_fifo.sv
@@ -3,8 +3,12 @@
 module tb_fifo();
     import tb_fifo_pkg::*;
     logic clk;
-
     logic result_flag;
+    logic wrclk = clk;
+    logic rdclk = clk;
+    logic rd_rst = 1;
+    logic wr_rst = 1;
+    int  i;
     data_t expected_value;
 	
     tester_fifo t;
@@ -17,57 +21,43 @@ module tb_fifo();
         .itf(itf.aFIFO)
     );
     
+    
     //1st case: Check equal
     initial begin
-	t = new (itf);
-        logic wrclk = clk;
-        logic rdclk = clk;
-        
+	t = new (itf);       
 
         //Write
-        for (int i = 0; i <= 10; i++) begin
+        for (i = 0; i <= 10; i++) begin
             #10 t.Push_Validation(.wrclk(wrclk), .wr_rst(wr_rst));
         end
       
         //Read
-        for (int i = 0; i <= 10; i++) begin
-            #10 t.Pop_validation(.rdclk(rdclk), .rd_rst_tb(rd_rst), .counter(i), .data_out(itf.data_out), .empty(itf.empty));
+        for (i = 0; i <= 10; i++) begin
+            #10 t.Pop_validation(.rdclk(rdclk), .rd_rst(rd_rst), .counter(i), .data_out(itf.data_out), .empty(itf.empty));
         end
     end
 
     //2nd case: Overflow
     initial begin
-	t = new (itf);
-        wrclk = clk;
-        rdclk = clk;
-        rd_rst = 1;
-        wr_rst = 1;
-
-        //Write
-        for (int i = 0; i <= 20; i++) begin
+	//Write
+        for (i = 0; i <= 20; i++) begin
             #10 t.Push_Validation(.wrclk(wrclk), .wr_rst(wr_rst));
         end
         
         //Read
-          for (int i = 0; i <= 20; i++) begin
+          for (i = 0; i <= 20; i++) begin
             #10 t.Pop_validation(.rdclk(rdclk), .rd_rst(rd_rst), .counter(i), .data_out(itf.data_out), .empty(itf.empty));
         end
     end
 
     //3rd case: Underflow
     initial begin
-	t = new (itf);
-        wrclk = clk;
-        rdclk = clk;
-        rd_rst_tb = 1;
-        wr_rst_tb = 1;
-
-        for (int i = 0; i <= 15; i++) begin
+        for (i = 0; i <= 15; i++) begin
             #10 t.Push_Validation(.wrclk(wrclk), .wr_rst(wr_rst));
         end
 
         //Read
-          for (int i = 0; i <= 20; i++) begin
+          for (i = 0; i <= 20; i++) begin
             #10 t.Pop_validation(.rdclk(rdclk), .rd_rst(rd_rst), .counter(i), .data_out(itf.data_out), .empty(itf.empty));
         end
 
